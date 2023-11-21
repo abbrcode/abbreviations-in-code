0f5ad5a30af4ecfc4851b7cb96213d4bed32f53b
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2023-11-21 16:59:13 +0100

    Linter sorts langs and translations

--- scripts/linter/index.mjs
+++ scripts/linter/index.mjs
@@ -4,7 +4,27 @@ import { readFileSync, writeFileSync } from 'fs';
 
 // Sort abbrs
 {
-   const abbrs = JSON.parse(readFileSync('./data/abbrs/.json', 'utf-8'));
+   const path = './data/abbrs/.json';
 
-   writeFileSync('./data/abbrs/.json', JSON.stringify(abbrs.sort(), null, 3), 'utf-8');
+   const abbrs = JSON.parse(readFileSync(path, 'utf-8'));
+
+   writeFileSync(path, JSON.stringify(abbrs.sort(), null, 3), 'utf-8');
+}
+
+// Sort langs
+{
+   const path = './data/langs.json';
+
+   const langs = JSON.parse(readFileSync(path, 'utf-8'));
+
+   writeFileSync(path, JSON.stringify(langs.sort(), null, 3), 'utf-8');
+}
+
+// Sort translations
+{
+   const path = './data/translations.json';
+
+   const translations = JSON.parse(readFileSync(path, 'utf-8'));
+
+   writeFileSync(path, JSON.stringify(translations.sort(), null, 3), 'utf-8');
 }
