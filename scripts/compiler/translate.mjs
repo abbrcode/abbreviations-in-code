d6d6a463c9588695b1a1988b51ed1d74c8a8b645
Tiziano <69322987+T1xx1@users.noreply.github.com>
2024-10-31 01:18:01 +0100

    Change: new compiler

--- scripts/compiler/translate.mjs
+++
@@ -1,9 +0,0 @@
-import Template from './template.mjs';
-
-export default function translate(lang) {
-   const template = new Template(lang);
-
-   template.list();
-
-   template.write(`./translations/${lang}.md`);
-};
