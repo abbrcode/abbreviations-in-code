2d04d68412dfc2fe5a3d4e81ebfdfce52a68951e
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2024-11-01 20:22:23 +0100

    Improv: format

--- scripts/types/i18n.ts
+++ scripts/types/i18n.ts
@@ -1,7 +1,8 @@
+type Lang = 'en' | 'it';
+
+export type Langs = Lang[];
+
 export type Translation = {
    word: string;
    translation: string | null;
 };
-
-type Lang = 'en' | 'it';
-export type Langs = Lang[];
